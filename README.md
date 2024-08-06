3dc683e6d423fbbb9d487607be3f0f653a2d28f2
Steve Chin <schin8@bu.edu>
2024-08-06 19:59:20 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Kotlin Spring Boot JPA
+
+Based on the Spring Guide, [Building web applications with Spring Boot and Kotlin](https://spring.io/guides/tutorials/spring-boot-kotlin).  
+
+---
+
+
+
+## Resources
+
+Git repo for the Spring Guild [tut-spring-boot-kotlin](https://github.com/spring-guides/tut-spring-boot-kotlin)
---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id("org.springframework.boot") version "3.3.2"
+    id("io.spring.dependency-management") version "1.1.6"
+    kotlin("plugin.jpa") version "1.9.24"
+    kotlin("jvm") version "1.9.24"
+    kotlin("plugin.spring") version "1.9.24"
+}
+
+group = "schin"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    val springBootStarterVersion = "3.3.2"
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:$springBootStarterVersion")
+    implementation("org.springframework.boot:spring-boot-starter-mustache:$springBootStarterVersion")
+    implementation("org.springframework.boot:spring-boot-starter-web:$springBootStarterVersion")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.1")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:2.0.0")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools:$springBootStarterVersion")
+
+    runtimeOnly("com.h2database:h2:2.2.224")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.3.1")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.9.25")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.3")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
